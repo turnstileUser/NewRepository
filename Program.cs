761a47673d1646d0ba3705ef447613c56d6bbfac
sankaranarayananns@outlook.com <sankaranarayananns@outlook.com>
2020-02-28 22:05:29 +0530

    conflict changes 4

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
 Console.WriteLine("Added in local repo");
      Console.WriteLine("Conflict check1");
 Console.WriteLine("Conflict change2");
+            Console.WriteLine("Conflict change4");
 
             Console.Read();
         }
